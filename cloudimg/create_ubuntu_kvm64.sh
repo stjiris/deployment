9dd42719633bd0ef68159607ecafb3c05eea3365
Flavio Martins <flaviomartins@acm.org>
2023-03-21 14:58:54 +0000

    small fix

--- cloudimg/create_ubuntu_kvm64.sh
+++ cloudimg/create_ubuntu_kvm64.sh
@@ -13,14 +13,13 @@ qm create $VMID --name $TEMPL_NAME --memory $MEM \
 qm set $VMID --args "-cpu kvm64,+cx16,+lahf_lm,+popcnt,+sse3,+ssse3,+sse4.1,+sse4.2"
 qm importdisk $VMID $IMG_NAME $DISK_STOR
 qm set $VMID --scsihw virtio-scsi-pci --virtio0 $DISK_STOR:$VMID/vm-$VMID-disk-0.raw
-qm set $VMID --serial0 socket
+qm set $VMID --serial0 socket --vga serial0
 qm set $VMID --ide2 $DISK_STOR:cloudinit
-qm set $VMID --boot c --bootdisk virtio0
+qm set $VMID --boot "order=virtio0;ide2;net0" --bootdisk virtio0
 
 qm set $VMID --agent 1
 qm set $VMID --hotplug disk,network,usb,memory,cpu
 qm set $VMID --vcpus 1
-qm set $VMID --vga qxl
 qm set $VMID --ipconfig0 ip=dhcp
 qm resize $VMID virtio0 $DISK_SIZE
 qm template $VMID
